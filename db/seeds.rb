1ea27dd72ea77bef61916cec52a0a8cd6ff59f2f
Monica Mateiu <monica.mateiu@gmail.com>
2021-04-06 20:47:13 +0100

    create user-course relationship and assign new course to current user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+User.create(email: 'admin@example.com', password: '123456', password_confirmation: '123456')
+
 30.times do
   Course.create!([{
     title: Faker::Educator.course_name,
-    description: Faker::TvShows::MichaelScott.quote
+    description: Faker::TvShows::MichaelScott.quote,
+    user_id: User.first.id
   }])
 end
